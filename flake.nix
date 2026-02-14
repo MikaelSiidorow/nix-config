5b14263483e1b186a78803667ece8c1fdc3bf072
Mikael Siidorow <mikael@siidorow.com>
2026-02-14 22:54:04 +0200

    format

--- flake.nix
+++ flake.nix
@@ -165,7 +165,8 @@
                 homeDirectory = "/home/${username}";
               };
             }
-          ] ++ extraModules;
+          ]
+          ++ extraModules;
         };
     in
     {
