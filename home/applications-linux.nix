2d3896e13aa0e67704b96704719a3a0a325844ac
Mikael Siidorow <mikael@siidorow.com>
2026-02-18 19:32:48 +0200

    fix(pop): fix steam env stuff, add stripe cli

--- home/applications-linux.nix
+++ home/applications-linux.nix
@@ -7,13 +7,14 @@
     # telegram-desktop - using flatpak instead due to graphics driver issues
 
     # Gaming
-    # steam - using apt version instead (better 32-bit lib support on Pop!_OS)
+    # steam - using apt version; wrapper below strips Nix PATH to avoid glibc conflicts
 
     # Productivity
     obsidian
 
     # Cloud
     azure-cli
+    stripe-cli
 
     # Document processing
     texliveFull
@@ -24,6 +25,18 @@
     # Terminal with nixGL wrapping (uses config.lib.nixGL.wrap)
     (config.lib.nixGL.wrap ghostty)
   ];
+
+  # Wrap apt Steam to use system PATH (Nix coreutils link to newer glibc than Pop!_OS ships)
+  xdg.desktopEntries.steam = {
+    name = "Steam";
+    comment = "Application for managing and playing games on Steam";
+    exec = ''env PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games" LD_LIBRARY_PATH="" /usr/games/steam %U'';
+    icon = "steam";
+    terminal = false;
+    type = "Application";
+    categories = [ "Game" "Network" ];
+    mimeType = [ "x-scheme-handler/steam" "x-scheme-handler/steamlink" ];
+  };
 
   programs.zed-editor = {
     enable = true;
